a98470298d5f8ff0b37a840d25f509f4bde442c3
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2022-08-06 22:08:53 -0500

    🚧 Extract 'url' column

--- py/scripts/holocene_hub.py
+++ py/scripts/holocene_hub.py
@@ -2,7 +2,7 @@
 import os.path
 
 # 🐍 External libaries
-# None
+from bs4 import BeautifulSoup, SoupStrainer
 
 # 🐍 Local module imports
 from src.html import save_html
@@ -11,12 +11,28 @@ from src.html import save_html
 html_url = "https://volcano.si.edu/volcanolist_holocene.cfm"
 html_directory = os.path.join("..", "data", "holocene_hub")
 html_filename = "hub.html"
+html_path = os.path.join(html_directory, html_filename)
 
-if os.path.exists(os.path.join(html_directory, html_filename)) == False:
+if os.path.exists(html_path) == False:
     save_html(
         url=html_url,
         directory=html_directory,
         filename=html_filename,
     )
 
-# 📝 Parse html file
+# 🍲 Make soup
+strainer = SoupStrainer(name="table", attrs={})
+
+with open(html_path, "r", encoding="utf-8") as file:
+    soup = BeautifulSoup(markup=file, features="lxml", parse_only=strainer)
+
+## ❄ Get rows
+rows = soup("tr")
+
+## 🌸 Extract "url" column
+base_url = "https://volcano.si.edu"
+urls = [
+    os.path.join(base_url, rows[i].contents[1].a.attrs["href"]) for i in range(1, 1338)
+]
+
+## ❄ Extract "volcano_number" column
