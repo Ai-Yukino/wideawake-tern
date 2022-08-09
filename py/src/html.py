508afa75f871f174d11d5ab07f0d8ea35f5d5a84
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2022-08-09 17:34:06 -0500

    🎨 Simplifly  by using standard library imports insteads of beautiful soup

--- py/src/html.py
+++ py/src/html.py
@@ -1,20 +1,12 @@
 # 🐍 Python standard library
-import urllib.request
-import os.path
+from urllib.request import urlretrieve
+from os.path import join
 
 # 🐍 External libaries
-from bs4 import BeautifulSoup
+# None
 
 # 🐍 Local module imports
 # None
 
-
 def save_html(url, directory, filename):
-    response = urllib.request.urlopen(url)
-    soup = BeautifulSoup(response, features="lxml")
-    html = str(soup)
-
-    path = os.path.join(directory, filename)
-    with open(path, "x", encoding="utf-8") as file:
-        file.write(html)
-        file.close()
+    urlretrieve(url, join(directory, filename))
