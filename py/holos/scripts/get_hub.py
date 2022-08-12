53430ee9d4cf83297eaff8c0a0b594c8150b81ea
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2022-08-12 17:07:11 -0500

    👩‍💻 Update script to use updated ai library

--- py/holos/scripts/get_hub.py
+++ py/holos/scripts/get_hub.py
@@ -1,31 +1,33 @@
 # 🐍 Python standard library
-import os
-import re
+from os.path import join, exists
+from os import makedirs
+
+from re import compile, search
 
 # 🐍 External libaries
 from bs4 import BeautifulSoup, SoupStrainer
 import pandas as pd
 
 # 🐍 Local module imports
-from src.html import save_html
+from ai.get import get_page
 
 # ❄ Create base variables
 base_url = "https://volcano.si.edu"
 base_name = "holocene_hub"
 
 # 🌸 Create data directory
-data_directory = os.path.join("..", "data")
+data_directory = join("..", "data")
 
-if os.path.exists(data_directory) == False:
-    os.makedirs(data_directory)
+if exists(data_directory) == False:
+    makedirs(data_directory)
 
 # ❄ Save html file
-html_url = os.path.join(base_url, "volcanolist_holocene.cfm")
-html_filename = base_name + ".html"
-html_path = os.path.join(data_directory, html_filename)
+html_url = join(base_url, "volcanolist_holocene.cfm")
+html_filename = base_name
+html_path = join(data_directory, html_filename + ".html")
 
-if os.path.exists(html_path) == False:
-    save_html(
+if exists(html_path) == False:
+    get_page(
         url=html_url,
         directory=data_directory,
         filename=html_filename,
@@ -41,14 +43,14 @@ with open(html_path, "r", encoding="utf-8") as file:
 rows = soup("tr")[1:]
 
 # 🌸 Extract columns
-urls = [os.path.join(base_url, row.contents[1].a.attrs["href"]) for row in rows]
-volcano_numbers = [int(re.search("\d{6}", url)[0]) for url in urls]
+urls = [join(base_url, row.contents[1].a.attrs["href"]) for row in rows]
+volcano_numbers = [int(search("\d{6}", url)[0]) for url in urls]
 
 names = [row.contents[1].string for row in rows]
 subregions = [row.contents[3].string for row in rows]
 volcano_types = [row.contents[5].string for row in rows]
 evidences = [
-    re.compile(
+    compile(
         r"^ ",
     ).sub("", row.contents[7].string)
     for row in rows
@@ -65,7 +67,7 @@ dictionary = {
 }
 df = pd.DataFrame(dictionary)
 
-tsv_path = os.path.join(data_directory, base_name + ".tsv")
+tsv_path = join(data_directory, base_name + ".tsv")
 
-if os.path.exists(tsv_path) == False:
+if exists(tsv_path) == False:
     df.to_csv(tsv_path, sep="\t", index=False)
