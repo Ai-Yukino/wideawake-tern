6365b3b27702d7abaefa7d120ec905da9a969760
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2022-08-18 16:08:00 -0500

    🚧 Add empty DataFrame

--- py/holos/scripts/parse_profiles.py
+++ py/holos/scripts/parse_profiles.py
@@ -1,10 +1,12 @@
 # 🐍 Python standard library
+from dataclasses import field
 from os.path import join
 from glob import glob
 from random import seed, choices
 
 # 🐍 External libraries
 from bs4 import BeautifulSoup, SoupStrainer
+import pandas as pd
 
 # 🐍 Local module imports
 # None
@@ -12,3 +14,30 @@ from bs4 import BeautifulSoup, SoupStrainer
 paths = glob(join("..", "data", "volcano_pages", "*"))
 seed("Blooming in the mud by Wolpis Carter")
 test_paths = choices(paths, k=1)
+
+df = pd.DataFrame(
+    columns=[
+        "volcano_number",
+        "name",
+        "country",
+        "primary_volcano_type",
+        "last_known_eruption",
+        "latitude",
+        "longitude",
+        "summit_elevation_meters",
+        "summit_elevation_feet",
+    ]
+)
+
+for path in test_paths:
+    with open(path, "r") as file:
+        soup = BeautifulSoup(
+            markup=file,
+            features="lxml",
+            parse_only=SoupStrainer(
+                name="div", attrs={"id": "ProfileHolocene", "class": "section"}
+            ),
+        )
+        file.close()
+
+    name = ""
