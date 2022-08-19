e62c3caae00a2745c71fe553952570f70c1c06a1
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2022-08-19 14:05:40 -0500

    🚧 Start utility functions for polars-based testing

--- py/ai/testing.py
+++ py/ai/testing.py
@@ -6,20 +6,23 @@ Utilities functions to help with unit tests using pandas
 # None
 
 # 🐍 External libraries
-import pandas as pd
+# import pandas as pd
+import polars as pl
 
 # 🐍 Local module imports
 # None
 
-
 def assert_rows_subset(subset, superset):
-    """
-    Checks if the set of rows of one DataFrame
-    are a subset of another DataFrame's set of rows.
-    """
-    subset = subset.drop_duplicates()
-    superset = superset.drop_duplicates()
-    if subset.shape[1] != superset.shape[1]:
-        return False
-    else:
-        return subset.merge(superset).shape == subset.shape
+    pass
+
+# def assert_rows_subset(subset, superset):
+#     """
+#     Checks if the set of rows of one DataFrame
+#     are a subset of another DataFrame's set of rows.
+#     """
+#     subset = subset.drop_duplicates()
+#     superset = superset.drop_duplicates()
+#     if subset.shape[1] != superset.shape[1]:
+#         return False
+#     else:
+#         return subset.merge(superset).shape == subset.shape
