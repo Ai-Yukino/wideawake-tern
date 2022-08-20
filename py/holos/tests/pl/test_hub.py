bff6ee62e5f04702533447d1d2c710e764ae7a68
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2022-08-19 19:13:52 -0500

    🤏 Fix typo

--- py/holos/tests/pl/test_hub.py
+++ py/holos/tests/pl/test_hub.py
@@ -12,7 +12,7 @@ from ai.testing import assert_frame_subset
 class TestHub(unittest.TestCase):
     def setUp(self):
         self.hub = pl.scan_csv(join("..", "..", "data", "hub.tsv"), sep="\t")
-        self.sample = pl.scan.csv(join("samples", "hub.tsv"), sep="\t")
+        self.sample = pl.scan_csv(join("samples", "hub.tsv"), sep="\t")
 
     def test_subset(self):
         self.assertEqual(assert_frame_subset(self.sample, self.hub), None)
