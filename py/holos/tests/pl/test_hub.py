ff802e0accded3b12f81d2edd63f69b36633a78e
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2022-08-19 19:19:05 -0500

    🧪 Add dataframe shape test

--- py/holos/tests/pl/test_hub.py
+++ py/holos/tests/pl/test_hub.py
@@ -14,6 +14,9 @@ class TestHub(unittest.TestCase):
         self.hub = pl.scan_csv(join("..", "..", "data", "hub.tsv"), sep="\t")
         self.sample = pl.scan_csv(join("samples", "hub.tsv"), sep="\t")
 
+    def test_shape(self):
+        self.assertEqual(self.hub.shape, (1331, 6))
+
     def test_subset(self):
         self.assertEqual(assert_frame_subset(self.sample, self.hub), None)
 
--- py/holos/tests/pl/test_profiles.py
+++ py/holos/tests/pl/test_profiles.py
@@ -14,6 +14,9 @@ class TestHub(unittest.TestCase):
         self.hub = pl.scan_csv(join("..", "..", "data", "profiles.tsv"), sep="\t")
         self.sample = pl.scan_csv(join("samples", "profiles.tsv"), sep="\t")
 
+    def test_shape(self):
+        self.assertEqual(self.hub.shape, (1331, 9))
+
     def test_subset(self):
         self.assertEqual(assert_frame_subset(self.sample, self.hub), None)
 
