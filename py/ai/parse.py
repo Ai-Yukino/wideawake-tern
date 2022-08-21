ca5edb75eef99803562e3a2dc86bc349ddcfcc4c
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2022-08-21 14:26:55 -0500

    👩‍💻Add utility function null()

---
+++ py/ai/parse.py
@@ -0,0 +1,13 @@
+# 🐍 Python standard library
+# None
+
+# 🐍 External libraries
+# None
+
+# 🐍 Local module imports
+# None
+
+
+def null(value, new_value=None):
+    """Replace 'value' with 'new_value'."""
+    return new_value
