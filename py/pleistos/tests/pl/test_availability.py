d846b91b3dd09f0ff802b728b6e3ccf03a935b3d
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2022-09-01 22:49:10 -0500

    🧪 Add test for availability.tsv

---
+++ py/pleistos/tests/pl/test_availability.py
@@ -0,0 +1,30 @@
+# 🐍 Python standard library
+import unittest
+from os.path import join
+
+# 🐍 External libraries
+import polars as pl
+
+# 🐍 Local module imports
+from ai.testing import assert_frame_subset
+
+
+class TestHub(unittest.TestCase):
+    def setUp(self):
+        self.availability = pl.scan_csv(
+            join("..", "..", "data", "tsv", "availability.tsv"), sep="\t"
+        )
+        self.samples = pl.scan_csv(join("samples", "tsv", "availability.tsv"), sep="\t")
+
+    def test_shape(self):
+        self.assertEqual(self.profiles.collect().shape, (1331, 9))
+
+    def test_subset(self):
+        self.assertEqual(
+            assert_frame_subset(self.samples, self.profiles),
+            None,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
