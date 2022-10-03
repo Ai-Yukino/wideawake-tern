f1699b30181537963584ac107669fa4f1e41197a
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2022-10-03 07:49:45 -0500

    👩‍💻 Simplify get_page() and get_pages() using the @timer annotation

--- py/ai/src/get.py
+++ py/ai/src/get.py
@@ -5,31 +5,22 @@ Utilities functions for dealing with get requests in web scraping
 # 🐍 Python standard library
 from urllib.request import urlretrieve, urlcleanup
 from os.path import join
-from time import time
 import csv
 from random import choices, seed
 
-# 🐍 Python standard library
+# 🐍 External libraries
 import requests
 
-# 🐍 Python standard library
-# None
+# 🐍 Local modules
+from ai.src.logging import timer
 
-
+@timer
 def get_page(url, directory, filename, filename_extension=".html"):
     """Save a single static web page"""
-    start = time()
-
     urlretrieve(url, join(directory, filename + filename_extension))
     urlcleanup()
 
-    end = time()
-    print("\n---")
-    print(f"url: {url}")
-    print(f"get_page() ran for {end - start} seconds")
-    print("---")
-
-
+@timer
 def get_pages(
     urls,
     directory,
@@ -38,8 +29,7 @@ def get_pages(
     sample_size=-1,
     sample_seed="uwu",
 ):
-    "Same multiple static web pages"
-    function_start = time()
+    "Save multiple static web pages"
 
     if 0 < sample_size < len(urls):
         seed(sample_seed)
@@ -48,25 +38,12 @@ def get_pages(
         filenames = [filenames[index] for index in sample_indices]
 
     with requests.Session() as s:
-        i = 1
         for url, filename in zip(urls, filenames):
-            start = time()
             with requests.Session() as s:
                 r = s.get(url)
                 path = join(directory, filename + filename_extension)
                 with open(path, "x") as file:
                     file.write(r.text)
-            end = time()
-
-            print("\n---")
-            print(f"Iteration: {i}")
-            print(f"url: {url}")
-            print(f"Time elapsed: {end - start}")
-            print("---")
-            i += 1
-
-    function_end = time()
-    print(f"get_pages() ran for {function_end - function_start} seconds")
 
 
 def get_column(path, column_index, sep="\t"):
