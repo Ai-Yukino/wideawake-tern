af8809b9da50e5b1a77931a61475dbc3dfb8959c
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2022-10-04 13:54:40 -0500

    🎨 Shorten variable names and hardcode '.html' extension

--- py/ai/src/get.py
+++ py/ai/src/get.py
@@ -25,19 +25,20 @@ def save_html(url, dir, name):
 @timer
 def session_save_html(
     urls,
-    directory,
-    filenames,
-    filename_extension=".html",
+    dir,
+    names,
 ):
-    "Save multiple static web pages using a single persistent HTTP connection"
+    """Use a single persistent HTTP connection to
+    save multiple html pages from `urls` to
+    a local html files `names` at `dir`"""
 
     with requests.Session() as s:
-        for url, filename in zip(urls, filenames):
+        for url, name in zip(urls, names):
             with requests.Session() as s:
                 r = s.get(url)
-                path = join(directory, filename + filename_extension)
-                with open(path, "x") as file:
-                    file.write(r.text)
+                path = join(dir, name + ".html")
+                with open(path, "x") as f:
+                    f.write(r.text)
 
 
 def get_column(path, column_index, sep="\t"):
