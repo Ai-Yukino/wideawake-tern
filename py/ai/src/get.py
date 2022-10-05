bfea5bf625e3abf6196878f6c2c8a7e892ceef90
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2022-10-04 19:34:54 -0500

    🎨 Rename 'name' variables to 'filename'

--- py/ai/src/get.py
+++ py/ai/src/get.py
@@ -15,10 +15,10 @@ from ai.src.logging import timer
 
 
 @timer
-def save_html(url, dir, name):
+def save_html(url, dir, filename):
     """Save an html page from `url` to
     a local html file `name` at `dir`."""
-    urlretrieve(url, join(dir, name + ".html"))
+    urlretrieve(url, join(dir, filename + ".html"))
     urlcleanup()
 
 
@@ -26,17 +26,17 @@ def save_html(url, dir, name):
 def session_save_html(
     urls,
     dir,
-    names,
+    filenames,
 ):
     """Use a single persistent HTTP connection
     to save multiple html pages from `urls` to
     local html files `names` at `dir`"""
 
     with requests.Session() as s:
-        for url, name in zip(urls, names):
+        for url, filename in zip(urls, filenames):
             with requests.Session() as s:
                 r = s.get(url)
-                path = join(dir, name + ".html")
+                path = join(dir, filename + ".html")
                 with open(path, "x") as f:
                     f.write(r.text)
 
