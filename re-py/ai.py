9ceeec692dde7a072a791a46e06c73f51d5664cb
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2022-10-09 17:57:44 -0500

    👩‍💻 Create basic struction for main ai.py script

---
+++ re-py/ai.py
@@ -0,0 +1,29 @@
+"""{Name TBA}
+
+{Description TBA}
+"""
+
+## 🐍 Python standard library
+# None
+
+## 🐍 External packages
+# None
+
+## 🐍 Local modules
+from src.yukino import time_this
+
+
+## Main function
+@time_this
+def ai():
+    umu()
+
+
+## Other functions
+@time_this
+def umu():
+    print("umu")
+
+
+if __name__ == "__main__":
+    ai()
