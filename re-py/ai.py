babb11bd9b7d1f4d8f703fc6572ab50e6dd4073e
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2022-10-11 00:28:47 -0500

    👩‍💻 Parse database version  and start prototyping code out of the ai() function

--- re-py/ai.py
+++ re-py/ai.py
@@ -17,18 +17,22 @@ from bs4 import BeautifulSoup, SoupStrainer
 ## 📝 Main function
 def ai():
     ### 📝 Save "Database Updates" page
-    makedirs(join("data", "html"))
-    makedirs(join("data", "tsv"))
-    r = requests.get("https://volcano.si.edu/gvp_votw.cfm")
-    with open(join("data", "html", "db_info.html"), "x") as f:
-        f.write(r.text)
-        f.close()
-
-    ### 📝
-    with open(join("data", "html", "db_info.html"), "r") as f:
-        soup = BeautifulSoup(markup=f, features="lxml", parse_only=SoupStrainer())
-        f.close()
+    # makedirs(join("data", "html"))
+    # makedirs(join("data", "tsv"))
+    # r = requests.get("https://volcano.si.edu/gvp_votw.cfm")
+    # with open(join("data", "html", "db_info.html"), "x") as f:
+    #     f.write(r.text)
+    #     f.close()
+    pass
 
 
 if __name__ == "__main__":
-    ai()
+    # ai()
+    with open(join("data", "html", "db_info.html"), "r") as f:
+        soup = BeautifulSoup(
+            markup=f, features="lxml", parse_only=SoupStrainer(name="tbody")
+        )
+        table = soup.contents[1]
+        rows = table("td")
+        f.close()
+    db_version = str(rows[0].string)
